9f4b84aa4c5f4e63de7d81a409902c19ad334b5b
hyojeongindeyo <ghdgywjd510@naver.com>
2023-07-04 22:32:43 +0900

    hi

--- config/settings.py
+++ config/settings.py
@@ -10,9 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+# 지연언니는 천재 , 나는 뭐지,,,,,,, 나는 그냥 행인 1.....
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
