0c59dcb4c0834bd33ec13731cbae20c286dcc777
KimJiYeon <jsujyeon@naver.com>
2023-07-06 13:20:14 +0900

    update4

--- yogithon/forms.py
+++ yogithon/forms.py
@@ -4,7 +4,7 @@ from .models import Pet, User
 class PetForm(forms.ModelForm):
     class Meta :
         model = Pet
-        fields = [
+        fields = (
             'title',
             'p_name',
             'school',
@@ -12,15 +12,15 @@ class PetForm(forms.ModelForm):
             'description',
             'image',
             'insta_url'
-        ]
+        )
 
 class UserForm(forms.ModelForm) :
     class Meta :
         model = User
-        fields = [
+        fields = (
             'first_name',
             'last_name',
             'user_id',
             'user_pw',
             'email'
-        ]
+        )
