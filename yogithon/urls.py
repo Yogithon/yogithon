a044bf5c4d2fded8b4dbe6587bd53d628a5577cf
ekdms <reyna11@naver.com>
2023-07-08 02:27:18 +0900

    detail.html에 css 적용했는데 아주 살짝 오류있음

--- yogithon/urls.py
+++ yogithon/urls.py
@@ -6,10 +6,10 @@ app_name = "yogithon"
 
 urlpatterns = [
     path('', views.main, name="home"),
-    path('<int:pk>/', views.detail, name="detail"),
+    path('detail/<int:pk>/', views.detail, name="detail"),
     path('new/', views.new, name="new"),
     path('create/', views.create, name="create"),
-    path('<int:pk>/update/', views.update, name="update"),
+    path('update/<int:pk>/', views.update, name="update"),
     path('<int:pk>/delete/', views.delete, name="delete"),
     path('login/github/', views.github_login, name='github-login'),
     path('login/github/callback/', views.github_login_callback, name='github-callback'),
